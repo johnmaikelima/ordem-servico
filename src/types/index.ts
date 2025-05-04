27a999a459ae4455e5d3ab9eda7c1ed8e5157095
johnmaikelima <johnpatrocinio1@gmail.com>
2025-05-03 22:19:48 -0300

    Atualizada interface Cliente para usar objeto Endereco

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,20 @@
+export interface Endereco {
+  rua: string;
+  numero: string;
+  complemento?: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+}
+
 export interface Cliente {
   _id: string;
   nome: string;
   email: string;
   telefone: string;
   cpfCnpj: string;
-  endereco: string;
+  endereco: Endereco;
 }
 
 export interface Prestador {
